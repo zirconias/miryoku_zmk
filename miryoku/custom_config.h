66833a4895182fc71409df1d24c04016775fdddc
islam mohamed drissi <i.slam.drissi@gmail.com>
2026-04-14 23:58:02 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,15 +3,9 @@
 
 #pragma once
 
-// Declare BT behaviors (required for &bt BT_CLR to work)
+// Minimal BT‑clear key on the Media layer
 #define MIRYOKU_KLUDGE_LAYERS \
   X(Media)
 
-// Map a temporary BT_CLR on the Media layer only
-// This does NOT break the core Miryoku mapping
 #define MIRYOKU_LAYER_MEDIA \
   &bt BT_CLR
-
-// Optional: also add BT_NXT, BT_PRV if you want profile switching later
-// #define MIRYOKU_LAYER_MEDIA \
-//   &bt BT_CLR, &bt BT_NXT, &bt BT_PRV
