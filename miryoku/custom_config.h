dfeaa1414e2852bb2ed63d56751f44f4339e4ec4
islam mohamed drissi <i.slam.drissi@gmail.com>
2026-04-15 00:14:17 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,3 @@
 // https://github.com/manna-harbour/miryoku
 
 #pragma once
-
-// Add a Media layer with BT_CLR; no multi‑line macros
-#define MIRYOKU_KLUDGE_LAYERS X(Media)
-#define MIRYOKU_LAYER_MEDIA    &bt BT_CLR
