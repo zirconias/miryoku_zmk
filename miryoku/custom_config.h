e0c1e7c65f68ebb39021ae3d8fb868c87ab976a0
islam mohamed drissi <i.slam.drissi@gmail.com>
2026-04-14 23:34:00 +0200

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,20 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#pragma once
+
+#if defined(MIRYOKU_KEYBOARD_CORNE)
+
+#undef MIRYOKU_LAYOUTMAPPING_CORNE
+#define MIRYOKU_LAYOUTMAPPING_CORNE( \
+K00, K01, K02, K03, K04, K05,                    K06, K07, K08, K09, K10, K11, \
+K12, K13, K14, K15, K16, K17,                    K18, K19, K20, K21, K22, K23, \
+K24, K25, K26, K27, K28, K29,                    K30, K31, K32, K33, K34, K35, \
+               K36, K37, K38, K39,    K40, K41, K42, K43 \
+) \
+&kp ESC K00 K01 K02 K03 K04                       K06 K07 K08 K09 K10 K11 \
+&bt BT_CLR K12 K13 K14 K15 K16                    K18 K19 K20 K21 K22 K23 \
+XXX K24 K25 K26 K27 K28                           K30 K31 K32 K33 K34 XXX \
+               K36 K37 K38 K39                    K40 K41 K42 K43
+
+#endif
